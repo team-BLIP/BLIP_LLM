3cce78200e08ececd3ac3ff6da52ade4e1645c4e
hyohyeon08 <hyohyeonkim08@gmail.com>
2025-03-19 18:23:19 +0900

    .

--- src/main.py
+++ src/main.py
@@ -1,17 +1,8 @@
 from src.router import meeting_router
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(meeting_router.router)
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 
 @app.get('/')
